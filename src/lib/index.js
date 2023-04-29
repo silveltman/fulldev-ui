94683bce9587e0fc8af5e67327edb3b2fc659ada
silveltman <52583398+silveltman@users.noreply.github.com>
2023-04-29 19:48:17 +0200

    component exports 0.4.1

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,14 @@
 // Base
 export { default as Button } from 'base/Button.svelte'
 export { default as Card } from 'base/Card.svelte'
-export { default as Disclosure } from 'base/Disclosure.svelte'
+export { default as Checkbox } from 'base/Checkbox.svelte'
+export { default as Collapse } from 'base/Collapse.svelte'
 export { default as Eyebrow } from 'base/Eyebrow.svelte'
 export { default as Form } from 'base/Form.svelte'
 export { default as Image } from 'base/Image.svelte'
-export { default as Link } from 'base/Link.svelte'
 export { default as Logo } from 'base/Logo.svelte'
 export { default as Person } from 'base/Person.svelte'
+export { default as Radio } from 'base/Radio.svelte'
 export { default as Select } from 'base/Select.svelte'
 export { default as Social } from 'base/Social.svelte'
 export { default as Textarea } from 'base/Textarea.svelte'
@@ -16,19 +17,23 @@ export { default as Whatsapp } from 'base/Whatsapp.svelte'
 
 // Slot
 export { default as SlotAccordion } from 'slot/SlotAccordion.svelte'
-export { default as SlotCards } from 'slot/SlotCardGrid.svelte'
+export { default as SlotCardCarousel } from 'slot/SlotCardCarousel.svelte'
+export { default as SlotCardGrid } from 'slot/SlotCardGrid.svelte'
+export { default as SlotCardMasonry } from 'slot/SlotCardMasonry.svelte'
 export { default as SlotChecklist } from 'slot/SlotChecklist.svelte'
+export { default as SlotGallery } from 'slot/SlotGallery.svelte'
 export { default as SlotHighlight } from 'slot/SlotHighlight.svelte'
-export { default as SlotMedia } from 'slot/SlotMedia.svelte'
 export { default as SlotNewsletter } from 'slot/SlotNewsletter.svelte'
 export { default as SlotRichtext } from 'slot/SlotRichtext.svelte'
 export { default as SlotStats } from 'slot/SlotStats.svelte'
 
-// Block
-export { default as BlockCta } from 'block/BlockCta.svelte'
-export { default as BlockFaq } from 'block/BlockFaq.svelte'
-export { default as BlockFeatures } from 'block/BlockFeatures.svelte'
-export { default as BlockCards } from 'block/BlockCards.svelte'
+// Layout
+export { default as Container } from 'layout/Container.svelte'
+export { default as Section } from 'layout/Section.svelte'
+export { default as SectionBox } from 'layout/SectionBox.svelte'
+export { default as SectionFill } from 'layout/SectionFill.svelte'
+export { default as SectionSplit } from 'layout/SectionSplit.svelte'
+export { default as SectionStack } from 'layout/SectionStack.svelte'
 
-export { default as TheHeader } from 'block/TheHeader.svelte'
-export { default as TheFooter } from 'block/TheFooter.svelte'
+
+
