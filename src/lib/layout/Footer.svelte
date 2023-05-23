8c4018cedb75f85ec161df9404c2df46d9e9791c
silveltman <52583398+silveltman@users.noreply.github.com>
2023-05-23 13:45:24 +0200

    c

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import Container from 'layout/Container.svelte';
+	import Split from './Split.svelte';
 
 	let className = '';
 	export { className as class };
 </script>
 
-<footer class="border-t border-base-6 bg-base-2 py-lg {className}">
+<footer class="border-t border-base-6 bg-base-2 py-xl {className}">
 	<Container>
-		<div
-			class="grid gap-2xl sm:grid-cols-[repeat(auto-fit,minmax(34%,1fr))] lg:grid-cols-[repeat(auto-fit,minmax(21%,1fr))]"
-		>
-			<slot />
-		</div>
+		<Split>
+			<slot name="start" />
+			{#if $$slots.lists}
+				<div class="columns-[160px] gap-x-lg space-y-lg [&>*]:break-inside-avoid-column">
+					<slot name="lists" />
+				</div>
+			{/if}
+			<slot name="end" />
+		</Split>
 		<div>
 			<slot name="bottom" />
 		</div>
