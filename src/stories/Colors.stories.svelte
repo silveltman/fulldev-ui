c8a4a7f38548e84008af06c568d760d808e893f3
silveltman <52583398+silveltman@users.noreply.github.com>
2023-05-24 23:13:00 +0200

    design system stories added

---
+++ src/stories/Colors.stories.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta title="Design system/Colors" />
+
+<Template>
+	<div class="flex flex-col gap-2">
+		<div class="h-10 w-full bg-base-1" />
+		<div class="h-10 w-full bg-base-2" />
+		<div class="h-10 w-full bg-base-3" />
+		<div class="h-10 w-full bg-base-4" />
+		<div class="h-10 w-full bg-base-5" />
+		<div class="h-10 w-full bg-base-6" />
+		<div class="h-10 w-full bg-base-7" />
+		<div class="h-10 w-full bg-base-8" />
+		<div class="h-10 w-full bg-base-9" />
+		<div class="h-10 w-full bg-base-10" />
+		<div class="h-10 w-full bg-base-11" />
+		<div class="h-10 w-full bg-base-12" />
+	</div>
+</Template>
+
+<Story name="Default" />
---
+++ src/stories/Spacing.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta title="Design system/Spacing" />
+
+<Template>
+	<div class="flex flex-col gap-2">
+		<div class="my-[10px] h-10 w-xs bg-base-9" />
+		<div class="my-[10px] h-10 w-sm bg-base-9" />
+		<div class="my-[10px] h-10 w-md bg-base-9" />
+		<div class="my-[10px] h-10 w-lg bg-base-9" />
+		<div class="my-[10px] h-10 w-xl bg-base-9" />
+		<div class="my-[10px] h-10 w-2xl bg-base-9" />
+	</div>
+</Template>
+
+<Story name="Default" />
